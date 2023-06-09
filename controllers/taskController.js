9d6c83e137b43204130802e58a6e14c4ef6b219d
Chandan Janardan <chandanjanardan@gmail.com>
2023-06-09 05:46:30 +0530

    adding delete controller

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -30,22 +30,31 @@ const getTask=async(req,res)=>{
     try {
         const {id:taskID}=req.params
         const task = await Task.findOne({_id:taskID})
-    if(!task){
+    if(!taskID){
         return res.status(404).json({msg:`No task with id ${taskID}`})
     }
     res.status(200).json({task})
-
-
-        
     } catch (error) {
-        
+       res.status(500).json({msg:error}) 
     }
 }
 const updateTask=(req,res)=>{
     res.send("update task")
 }
-const deleteTask=(req,res)=>{
-    res.send("delete task")
+const deleteTask=async(req,res)=>{
+    try {
+        const {id:taskID}= req.params
+        const task= await Task.findOneAndDelete({_id:taskID})
+        console.log(`Task with id:${taskID} deleted`)
+        // res.status(200).json({msg:`${taskID} deleted`})
+        if(!task){
+            // rerturn is imp here
+            return res.status(404).json({msg:`No task with id ${taskID}`})
+        }
+        
+    } catch (error) {
+        res.status(500).json({msg:error}) 
+    }
 }
 
 
